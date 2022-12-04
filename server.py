901ca2f78d92fab7c1314c6c0f409cbf596a2a4f
arthurshmidt <arthur@arthurshmidt.com>
2022-12-03 23:54:11 -0500

    commments and cleanup

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import json
 global data 
 global stpt
 
+# Read in stpt from stpt.json and load them into stpt_json variable.  
 def read_stpt(stpt_json):
     
     with open('data/stpt.json','r') as openfile:
@@ -11,11 +12,14 @@ def read_stpt(stpt_json):
     print("startstop value: "+ str(stpt_json["startstop"]))
     return stpt_json
 
-def update_data(data_json):
+# Write data to data.json file.
+def write_data(data_json):
+    # Testing: increment value by 1.
     counter = data_json["value"] 
     data_json["value"] = data_json["value"] + 1
     print("update_data(): " + str(data_json["value"]))
 
+    # Write data_json to file.  
     json_object = json.dumps(data_json,indent=4)
     with open('data/data.json','w') as openfile:
         openfile.write(json_object)
@@ -24,7 +28,7 @@ def update_data(data_json):
 
 if __name__ == "__main__":
 
-    # counter
+    # Set initial values
     data = {
         "value": 0
     }
@@ -33,7 +37,8 @@ if __name__ == "__main__":
         "startstop": 0
     }
 
+    # Test loop
     while True:
         stpt = read_stpt(stpt)
-        data = update_data(data)
+        data = write_data(data)
         time.sleep(1)
